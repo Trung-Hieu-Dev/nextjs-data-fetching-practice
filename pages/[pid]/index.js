3371cde865a26d22b1f159663f58a5442054d9c2
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-06-22 16:52:27 +0700

    Loading Paths Dynamically

--- pages/[pid]/index.js
+++ pages/[pid]/index.js
@@ -12,13 +12,19 @@ function ProductDetailPage({ loadedProduct }) {
 
 export default ProductDetailPage;
 
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-data.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   const id = params.pid;
 
-  const filePath = path.join(process.cwd(), "data", "dummy-data.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === id);
 
@@ -30,12 +36,13 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+
+  const pathWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
-    ],
+    paths: pathWithParams,
     fallback: false,
   };
 }
